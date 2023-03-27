d25ee85c3f5fd55967631b8b85c07afbbfab5407
ktunprasert <ktunprasert@outlook.com>
2023-03-27 11:38:21 +0100

    etl: added

---
+++ etl/src/main/scala/Etl.scala
@@ -0,0 +1,6 @@
+object Etl {
+  def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] =
+    scoreMap
+      .flatMap { case (k, seq) => seq.map(v => (v.toLowerCase, k)) }
+      .to(Map)
+}
