f0b2ebfcd36c19073c54d908f7780180775f35b6
ktunprasert <ktunprasert@outlook.com>
2023-03-27 11:39:21 +0100

    etl: let type promotion handle it

--- etl/src/main/scala/Etl.scala
+++ etl/src/main/scala/Etl.scala
@@ -1,6 +1,4 @@
 object Etl {
   def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] =
-    scoreMap
-      .flatMap { case (k, seq) => seq.map(v => (v.toLowerCase, k)) }
-      .to(Map)
+    scoreMap.flatMap { case (k, seq) => seq.map(v => (v.toLowerCase, k)) }
 }
